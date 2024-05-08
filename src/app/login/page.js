0117e71479a7c658a592025d576f323a53d55978
Manideep Vaddepalli <manideepvaddepalli2@gmail.com>
2024-05-08 14:11:22 +0530

    second

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -9,42 +9,50 @@ import { browserName } from "react-device-detect";
 
 
 export default function Page() {
-  //checking if the app opened in instagram
-  if (browserName.includes("instagram") || browserName.includes("Instagram")){
-    const linkvar=""
-    useEffect(()=>{
-      document.getElementById("clicklink").click()
-    },[])
-    return(<><Header></Header><div className="insta-container"> 
-      <p>This app can't be displayed in instagram</p>
-      <h6>Click on below button</h6>
-      <a id="clicklink" href={linkvar} target="_blank" download>Open in browser</a>
-      </div></>)
-  }
   const router=useRouter()
   const {dispatch} = useAuthContext()
   const [datavar,useDatavar]=useState("")
-  function dataHandle(data){
-    useDatavar(data)
-    //storing used to context api to keep track
-    dispatch({type:'LOGIN',payload:data})
+  useEffect(()=>{
+    const linkelem = document.getElementById("clicklink")
+    if(linkelem){
+      linkelem.click()
+    }
+  },[])
+  //checking if the app opened in instagram
+  if (!browserName.includes("instagram") || !browserName.includes("Instagram")){
+    function dataHandle(data){
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+      useDatavar(data)
+      //storing used to context api to keep track
+      dispatch({type:'LOGIN',payload:data})
+    }
+    //storing login info to local storage --- can be hashed using bcrypt if any importatnt info
+    localStorage.setItem("user",JSON.stringify(datavar))
+    //if user logged pusing to homepage
+    if(datavar){
+        router.push("/")
+    }
+    return (<div className="login-page-container">
+      <Header></Header>
+    {!datavar&&
+    <div className="login-container">
+      <div className="login-card">
+      <h4>Select the Account</h4>
+      <Auth updateData = {dataHandle}></Auth>
+      </div>
+      </div>}
+      </div>
+    )
   }
-  //storing login info to local storage --- can be hashed using bcrypt if any importatnt info
-  localStorage.setItem("user",JSON.stringify(datavar))
-  //if user logged pusing to homepage
-  if(datavar){
-      router.push("/")
-  }
-  return (<div className="login-page-container">
-    <Header></Header>
-  {!datavar&&
-  <div className="login-container">
-    <div className="login-card">
-    <h4>Select the Account</h4>
-    <Auth updateData = {dataHandle}></Auth>
-    </div>
-    </div>}
-    </div>
-  )
+  const linkvar=""
+  return(<><Header></Header><div className="insta-container"> 
+    <p>This app cant be displayed in instagram</p>
+    <h6>Click on below button</h6>
+    <a id="clicklink" href={linkvar} target="_blank" download>Open in browser</a>
+    </div></>)
+
   }
   
+
+
+ 
